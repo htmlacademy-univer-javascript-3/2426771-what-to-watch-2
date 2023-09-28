3eac0fbab164e97a9a5f08e9aeca2cff714fbc8c
VitaliyKosh <zs.101101@yandex.ru>
2023-09-29 01:52:10 +0300

    error TS2322 fixed

--- src/components/limited-route/limited-route.tsx
+++ src/components/limited-route/limited-route.tsx
@@ -1,12 +1,18 @@
-import {FC, ReactNode} from 'react';
+import {FC} from 'react';
 import { Navigate } from 'react-router-dom';
 
 type LimitedRouteProps = {
     redirectCondition: boolean;
     redirectTo: string;
-    children: ReactNode;
+    children: React.ReactNode;
 }
 
-const LimitedRoute: FC<LimitedRouteProps> = ({redirectCondition, redirectTo, children}) => redirectCondition ? <Navigate to={redirectTo}/> : children;
+const LimitedRoute: FC<LimitedRouteProps> = ({redirectCondition, redirectTo, children}) => {
+  if (redirectCondition) {
+    return <Navigate to={redirectTo}/>;
+  } else {
+    return children;
+  }
+};
 
 export default LimitedRoute;
